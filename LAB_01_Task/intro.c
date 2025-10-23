5286ee19ed5beb35092bd94025495425dec2af65
codewithamit-prog <amitkumarthegamechanger@gmail.com>
2025-10-23 16:16:09 +0530

    added 2 files
    added intro.c , helloworld.c

---
+++ LAB_01_Task/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    printf("Amit Kumar\n");
+    printf("Roll No. : MEB25053\n");
+    printf("Department : Mechanical Engineering\n");
+    printf("Tezpur University\n");
+    return 0;
+}
